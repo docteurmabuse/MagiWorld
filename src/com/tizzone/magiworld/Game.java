8a1cdc7ef25958516a7d533e923cda3b7ae4c9bb
Laurent Tizzone <laurent.tizzone@gmail.com>
2019-09-25 09:24:18 +0200

    add behavior in Game: - method runGames(); - method runGame();

--- src/com/tizzone/magiworld/Game.java
+++ src/com/tizzone/magiworld/Game.java
@@ -1,16 +1,35 @@
 package com.tizzone.magiworld;
 
+import java.util.Scanner;
+
 public class Game {
+	private int qtyPlayer=2;
+
 	private Character character;
 
 	public Character getCharacter() {
 		return character;
 	}
 
-	public void displayCreationPlayer(){
-		System.out.println("Création du personnage du Joueur  1");
-	}
+	public void displayCreationPlayer(int nbPLayer){
+
+			System.out.println("Création du personnage du Joueur" + nbPLayer);
+		}
+
 	public void chooseCharacter(){
-		System.out.println("Veuillez choisir la classe de votre personnage (1: Guerrier, 2: Rôdeur, 3: Mage");
+
+		System.out.println("Veuillez choisir la classe de votre personnage (1: Guerrier, 2: Rôdeur, 3: Mage )");
+	}
+
+    public void runGames() {
+		for (int i=0;i<qtyPlayer;i++)
+		{
+			displayCreationPlayer(i);
+			runGame();
+		}
+    }
+
+	public void runGame() {
+		chooseCharacter();
 	}
 }
